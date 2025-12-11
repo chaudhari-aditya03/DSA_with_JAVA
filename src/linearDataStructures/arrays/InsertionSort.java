9aaad58c6d65081361deca0e41a0f8d485f1a1f7
Aditya Chaudhari <adityachaudhari312005@gmail.com>
2025-12-11 23:53:00 +0530

    Insertion Sort

--- src/linearDataStructures/arrays/InsertionSort.java
+++ src/linearDataStructures/arrays/InsertionSort.java
@@ -1,6 +1,6 @@
 package linearDataStructures.arrays;
 
-class InserttionSort {
+class InsertionSort {
     public void insertionSort(int[] arr) {
         int n = arr.length;
 
@@ -9,7 +9,8 @@ class InserttionSort {
             int j = i - 1;
 
             // Move elements greater than key to one position ahead
-            while (j >= 0 && arr[j] > key) {
+            while (j >= 0 && arr[j] > key)
+            {
                 arr[j + 1] = arr[j];
                 j--;
             }
